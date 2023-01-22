9859233baaecd3ab8939c307a30712a156de3109
Olmo Zavala <olmozavala@gmail.com>
2023-01-22 10:37:09 -0500

    Updates test files

--- tests/test_2.py
+++ tests/test_2.py
@@ -3,9 +3,9 @@ def test_basic_io(capsys):
     res = basic_io('../test_folder')
     assert res['number_files'] ==  6
     assert res['files'] ==  ['f1.txt', 'f2', 'f3.txt', 'f4.py', 'f5.csv', 'folder1']
-    assert res['files_or_folder'] ==  ['file', 'folder', 'file', 'file', 'file', 'folder']
+    assert res['file_or_folder'] ==  ['file', 'folder', 'file', 'file', 'file', 'folder']
 
     res = basic_io('../test_folder3')
     captured = capsys.readouterr()
-    assert captured.out == "Folder does not exist.\n"
+    assert captured.out.lower().find("Folder does not exist".lower()) != -1
 
